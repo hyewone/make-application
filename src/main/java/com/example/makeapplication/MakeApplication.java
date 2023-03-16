6e8349faf999d4bfd2e04959a54e34efb644c0e5
hyewone <45805188+hyewone@users.noreply.github.com>
2023-03-16 18:05:55 +0900

    Initial commit

---
+++ src/main/java/com/example/makeapplication/MakeApplication.java
@@ -0,0 +1,13 @@
+package com.example.makeapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MakeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MakeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/makeapplication/MakeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.makeapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MakeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
